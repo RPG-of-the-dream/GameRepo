5440c4d520b8ae460ce92c57094d54aa3a61e60c
RuhliadevOleksandr <sasharuhliadev@gmail.com>
2023-04-08 18:46:28 +0300

    Added item icons and types

---
+++ Assets/Scripts/Items/Enums/ItemRarity.cs
@@ -0,0 +1,12 @@
+namespace Items.Enums
+{
+    public enum ItemRarity
+    {
+        None = 0,
+        Trash = 1,
+        Common = 2,
+        Rare = 3,
+        Legendary = 4,
+        Epic = 5, 
+    }
+}
