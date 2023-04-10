06fcc7f9e02c748978754d1f6ecf3d1d15b7bf67
Oleh Papka <olegpapka2002@gmail.com>
2023-04-10 19:42:46 +0300

    Added scriptables and storage

---
+++ Assets/Scripts/Items/Scriptable/BaseItemScriptable.cs
@@ -0,0 +1,10 @@
+using Items.Data;
+using UnityEngine;
+
+namespace Items.Scriptable 
+{ 
+    public abstract class BaseItemScriptable : ScriptableObject
+    {
+        public abstract ItemDescriptor ItemDescriptor { get; }
+    }
+}
---
+++ Assets/Scripts/Items/Scriptable/WeaponScriptable.cs
@@ -0,0 +1,12 @@
+﻿using Items.Data;
+using UnityEngine;
+
+namespace Items.Scriptable
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "ItemsSystem/Weapon")]
+    public class WeaponScriptable : BaseItemScriptable
+    {
+        [SerializeField] private WeaponDescriptor _weaponDescriptor;
+        public override ItemDescriptor ItemDescriptor => _weaponDescriptor;
+    }
+}
