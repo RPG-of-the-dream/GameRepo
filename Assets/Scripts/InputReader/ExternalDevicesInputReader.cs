23de284c11a45d13bc0d3b2368dd5d296035c3b5
RuhliadevOleksandr <sasharuhliadev@gmail.com>
2023-04-29 18:33:52 +0300

    Added control for inventory UI

--- Assets/Scripts/InputReader/ExternalDevicesInputReader.cs
+++ Assets/Scripts/InputReader/ExternalDevicesInputReader.cs
@@ -1,12 +1,26 @@
+using System;
+using Core.Services.Updater;
 using UnityEngine;
 
 namespace InputReader
 {
-    public class ExternalDevicesInputReader : IEntityInputSource
+    public class ExternalDevicesInputReader : IEntityInputSource, IWindowsInputSource, IDisposable
     {
         public Vector2 Direction => new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         public bool Attack { get; private set; }
+        public event Action InventoryRequested;
 
+        public ExternalDevicesInputReader() => ProjectUpdater.Instance.UpdateCalled += OnUpdate;
         public void ResetOneTimeActions() => Attack = false;
+        
+        public void Dispose() => ProjectUpdater.Instance.UpdateCalled -= OnUpdate;
+
+        private void OnUpdate()
+        {
+            if(Input.GetKeyDown(KeyCode.I))
+            {
+                InventoryRequested?.Invoke();
+            }
+        }
     }
 }
--- Assets/Scripts/InputReader/GameUIInputView.cs
+++ Assets/Scripts/InputReader/GameUIInputView.cs
@@ -1,20 +1,31 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace InputReader
 {
-    public class GameUIInputView : MonoBehaviour, IEntityInputSource
+    public class GameUIInputView : MonoBehaviour, IEntityInputSource, IWindowsInputSource
     {
         [SerializeField] private Joystick _joystick;
         [SerializeField] private Button _attackButton;
+        [SerializeField] private Button _inventoryButton;
 
         public Vector2 Direction => new(_joystick.Horizontal, _joystick.Vertical);
         public bool Attack { get; private set; }
+        public event Action InventoryRequested;
         
         public void ResetOneTimeActions() => Attack = false;
 
-        private void Awake() => _attackButton.onClick.AddListener(() => Attack = true);
+        private void Awake()
+        {
+            _attackButton.onClick.AddListener(() => Attack = true);
+            _inventoryButton.onClick.AddListener(() => InventoryRequested?.Invoke());
+        }
 
-        private void OnDestroy() => _attackButton.onClick.RemoveAllListeners();
+        private void OnDestroy()
+        {
+            _attackButton.onClick.RemoveAllListeners();
+            _inventoryButton.onClick.RemoveAllListeners();
+        }
     }
 }
